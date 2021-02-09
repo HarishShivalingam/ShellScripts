b20eca1c2c996595493ba85f2119d68a48723bb4
harish <harishs_88#yahoo.in>
2021-02-09 13:15:28 +1300

    Adding sample.sh

--- sample.sh
+++ sample.sh
@@ -1,2 +1,10 @@
-echo Hello World
-2nd line
+echo "Hello World"
+echo "2nd line"
+
+firstline="Welcome to"
+secondline="DevOps"
+clourred="\e[31m"
+clolorgreen="\e[32m"
+clolorreset="\e[0m"
+
+echo -e "$firstline$clourred$secondline$clolorgreen$clolorreset"
