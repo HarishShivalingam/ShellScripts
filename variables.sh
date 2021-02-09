0a3a7b689e554744585f990cc21f7298acd1c277
harish <harishs_88#yahoo.in>
2021-02-10 07:25:09 +1300

    echo updated

--- variables.sh
+++ variables.sh
@@ -7,3 +7,5 @@ echo "Today's date is $date"
 # Two braces are used $(()) one for arithmetic and other for command
 add=$((2+5+123))
 echo add = $add
+#Variables should not start with number
+#Variables by default no data type
