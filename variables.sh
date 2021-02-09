b42aae6c147e72e8ef3f063265a8f964464ff66f
harish <harishs_88#yahoo.in>
2021-02-10 07:20:32 +1300

    echo updated

--- variables.sh
+++ variables.sh
@@ -1,5 +1,8 @@
-test
 echo="welcome to shell"
 #VAR=$(COMMAND)
 date=$(date +%F)
 echo "Todays date is $date"
+
+#Arithmetic Substitution
+# Two braces are used $(()) one for arithmetic and other for command
+add=$((2+5+123))
