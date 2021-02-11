af8d9a66ea7380034af45a45b2be8816db69cb98
harish <harishs_88#yahoo.in>
2021-02-12 11:17:17 +1300

    Sample roboshop project

--- roboshop/component/common.sh
+++ roboshop/component/common.sh
@@ -3,6 +3,6 @@
 #to be executed by root user
 USER_ID=$(id -u)
 if [ "$USER_ID" -ne 0 ]; then
-  echo -e "\e[31mYou must be a root user\e[0m"
+  echo -e "\e[1;31mYou must be a root user\e[0m"
   exit 1
 fi
