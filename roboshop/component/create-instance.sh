a66dc7cc672adccc4bc213d74725e676e0a71c37
harish <harishs_88#yahoo.in>
2021-02-16 11:41:22 +1300

    Sample roboshop project changes
    Signed-off-by: harish <harishs_88#yahoo.in>

---
+++ roboshop/component/create-instance.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+component=$1
+
+if [ -z "${component}" ]; then
+  echo "Need a Input of component Name"
+  exit 1
+fi
+
+aws ec2 run-instances --launch-template LaunchTemplateId=lt-08b96a82c41b602dd --tag-specifications "ResourceType=instance,Tags=[{key=Name,Value=${component}}]"
