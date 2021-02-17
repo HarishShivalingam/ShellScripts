e1cd37f25e3d36c7f02cc4975017a607ac97063d
harish <harishs_88#yahoo.in>
2021-02-18 10:00:57 +1300

    Sample roboshop project changes
    Signed-off-by: harish <harishs_88#yahoo.in>

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -12,7 +12,6 @@ STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}"
 if [ $STATE != "running" ]; then
   aws ec2 run-instances --launch-template LaunchTemplateId=lt-08b96a82c41b602dd --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
 fi
-
 IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 export component
 export IPADDRESS
