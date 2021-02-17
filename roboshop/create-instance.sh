20688075fa0cd5418c61e2ce1e965c6313209bb9
harish <harishs_88#yahoo.in>
2021-02-18 10:09:32 +1300

    Sample roboshop project changes
    Signed-off-by: harish <harishs_88#yahoo.in>

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -8,12 +8,12 @@ if [ -z "${component}" ]; then
 fi
 
 STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text)
-#STATE= $(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text)
-if [ $STATE != 'running' ]; then
+
+if [ "$STATE" != "running" ]; then
   aws ec2 run-instances --launch-template LaunchTemplateId=lt-08b96a82c41b602dd --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
 fi
 IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 export component
 export IPADDRESS
-envsubst <record.json >/tmp/${component}.json
+envsubst <record.json >/tmp/"${component}".json
 #aws route53 change-resource-record-sets --hosted-zone-id Z03472882U8QD5D0VHNE4 --change-batch file://tmp/${component}.json
