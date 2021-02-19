02389d4507fb5b61ee92f4307355bd5a8d11b0ed
harish <harishs_88#yahoo.in>
2021-02-20 11:38:02 +1300

    Inventory

---
+++ roboshop/component/mongodb.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+COMPONENT=mongodb
+
+source component/common.sh
+
+print "Setup Mongo" ""
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+stat $?
+
+print "Installing mongodb" "yum install -y mongodb-org"
+yum install -y mongodb-org
+
+print "Update mongodb conf" "sed"
+sed -i -e 's/127.0.0.0/' /etc/mongod.conf
+stat $?
+
+print "Start MongoDB Service" "systemctl restart mongod"
+systemctl start mongod
+systemctl enable mongod
+systemctl restart mongod
+stat $?
+
+print "MongoDB Schema" '"curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/03f2af34-e227-44b8-a9f2-c26720b34942/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+"'
+curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/03f2af34-e227-44b8-a9f2-c26720b34942/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+stat $?
+
+print "Load Schema" ""
+cd /tmp
+unzip mongodb. && mongo < catalogue.js && mongo < users.js
+stat $?
+
+
+
+#Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
+
+#then restart the service
+
+/etc/mongod.conf
+
+
+systemctl restart mongod
+
+curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/03f2af34-e227-44b8-a9f2-c26720b34942/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+
+# cd /tmp
+# unzip mongodb.zip
+# mongo < catalogue.js
+# mongo < users.js
