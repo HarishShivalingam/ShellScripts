1506e369976ceb4cc3603d9d44bc153bc986adf7
harish <harishs_88#yahoo.in>
2021-02-20 15:24:46 +1300

    Inventory

--- roboshop/component/catalogue.sh
+++ roboshop/component/catalogue.sh
@@ -20,10 +20,10 @@ mkdir catalogue
 cd catalogue
 unzip /tmp/catalogue.zip
 
-print "Install NodeJS dependencies" "npm install"
+print "Install NodeJS dependencies" "npm install && npm install  --unsafe-perm && exit"
 npm install  --unsafe-perm
-stat $?
 exit
+stat $?
 
 print " updating the IP address of MONGODB Server in systemd.service file" ""
 sed -i -e "s/MONGO_DNSNAME/mongodb.devops2021.tk/" /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
