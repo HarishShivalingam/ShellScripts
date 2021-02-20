fc5f6315424b3701e73e2180b0bb5fd0842eac65
harish <harishs_88#yahoo.in>
2021-02-21 10:42:36 +1300

    Inventory

--- roboshop/component/rabbitmq.sh
+++ roboshop/component/rabbitmq.sh
@@ -7,7 +7,7 @@ if [ $? -ne 0 ]; then
   print "Installing Erlang" "yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm -y"
   yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm -y
   stat $?
-if
+fi
 
 print "Setup YUM repositories for RabbitMQ" "curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
