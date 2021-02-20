92b467e9319e2020968dd14277276b4cf2804ed8
harish <harishs_88#yahoo.in>
2021-02-21 09:29:28 +1300

    Inventory

--- roboshop/component/shipping.sh
+++ roboshop/component/shipping.sh
@@ -8,15 +8,17 @@ print "Add roboshop user" "useradd roboshop"
 id roboshop || useradd roboshop
 stat $?
 
-print "Download the repo" ""
-
-cd /home/roboshop
+print "Download shipping code" "curl -s -L -o /tmp/shipping.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/9c06b317-6353-43f6-81e2-aa4f5f258b2d/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true""
 curl -s -L -o /tmp/shipping.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/9c06b317-6353-43f6-81e2-aa4f5f258b2d/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
-mkdir shipping
+stat $?
+print "Extracting code""unzip /tmp/shipping.zip"
+mkdir /home/roboshop/shipping
 cd shipping
 unzip /tmp/shipping.zip
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
+stat $?
+
+print "Maven compile code" "mvn clean package && mv target/shipping-1.0.jar shipping.jar"
+mvn clean package && mv target/shipping-1.0.jar shipping.jar
 
 print "Copy the service file and start the service" ""
 cp /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
