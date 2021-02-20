a02cdd53a3a80da469666eb906f9a5878d8a5ecd
harish <harishs_88#yahoo.in>
2021-02-21 09:25:29 +1300

    Inventory

---
+++ roboshop/component/shipping.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+COMPONENT=shipping
+source component/common
+
+print "Maven Installation" "yum install maven -y"
+
+print "Add roboshop user" "useradd roboshop"
+id roboshop || useradd roboshop
+stat $?
+
+print "Download the repo" ""
+
+cd /home/roboshop
+curl -s -L -o /tmp/shipping.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/9c06b317-6353-43f6-81e2-aa4f5f258b2d/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+mkdir shipping
+cd shipping
+unzip /tmp/shipping.zip
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
+
+print "Copy the service file and start the service" ""
+cp /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
+systemctl daemon-reload
+systemctl start shipping
+systemctl enable shipping
