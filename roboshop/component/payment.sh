a7f4c8ece547f25af1957194f791e93cdcd991e9
harish <harishs_88#yahoo.in>
2021-02-21 17:34:05 +1300

    Inventory

--- roboshop/component/payment.sh
+++ roboshop/component/payment.sh
@@ -14,7 +14,7 @@ print "Download the repo" 'curl -L -s -o /tmp/payment.zip "https://dev.azure.com
 curl -L -s -o /tmp/payment.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/cd32a975-ee45-4b3b-a08e-8e97c3ca7733/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 
 print "Extracting payment code" "mkdir -p /home/roboshop/payment"
-mkdir -p /home/roboshop/payment && cd /home/roboshop/payment && unzip /tmp/payment.zip
+mkdir -p /home/roboshop/payment && cd /home/roboshop/payment && unzip -o /tmp/payment.zip
 
 print "Install the dependencies" "cd /home/roboshop/payment && pip3 install -r requirements.txt "
 cd /home/roboshop/payment && pip3 install -r requirements.txt
